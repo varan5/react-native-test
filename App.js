875fc11c60ee5f29e0103f9ec3721b363c2baf10
Varan <varan.bhalla@gmail.com>
2022-07-18 17:42:18 +0530

    Added new text message in app.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Our app is running</Text>
+      <Text>Feature A added</Text>
       <StatusBar style="auto" />
     </View>
   );
